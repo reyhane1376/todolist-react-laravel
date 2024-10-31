2f43bd501d32fa7382006a3b00eddfec1637c052
reyhane <97376622+reyhane1376@users.noreply.github.com>
2024-10-31 19:57:35 +0330

    add tailwind and fix template

--- Front/src/App.jsx
+++ Front/src/App.jsx
@@ -1,33 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className="bg-gray-100">
+        <div className="flex items-center justify-center h-screen">
+            <div className="w-full px-4 py-8 mx-auto shadow lg:w-1/3  bg-white">
+                <div className="flex items-center mb-6">
+                    <h1 className="mr-6 text-4xl font-bold text-purple-600"> TO DO APP</h1>
+                </div>
+                <div className="relative">
+                    <input type="text" placeholder="What needs to be done today?"
+                    className="w-full px-2 py-3 border rounded outline-none border-grey-600" />
+                </div>
+                <ul className="list-reset">
+                    <li className="relative flex items-center justify-between px-2 py-6 border-b">
+                    <div>
+                        <input type="checkbox" className="" />
+                        <p  className="inline-block mt-1 ml-2 text-gray-600">Tailwind CSS To DO App List 1</p>
+                    </div>
+                    <button type="button" className="absolute right-0 flex items-center space-x-1">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5 text-blue-600" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
+                        </svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5 text-red-700" fill="none" viewBox="0 0 24 24"
+                        stroke="currentColor" strokeWidth="2">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+                        </svg>
+                    </button>
+                    </li>
+                    <li className="relative flex items-center justify-between px-2 py-6 border-b">
+                    <div>
+                        <input type="checkbox" checked onChange={() => {}} className="" />
+                        <p  className="inline-block mt-1 ml-2 text-gray-600 line-through">Tailwind CSS To DO App List 2</p>
+                    </div>
+                    <button type="button" className="absolute right-0 flex items-center  space-x-1">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5 text-blue-600" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
+                        </svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5 text-red-700" fill="none" viewBox="0 0 24 24"
+                        stroke="currentColor" strokeWidth="2">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+                        </svg>
+                    </button>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
     </>
   )
 }
