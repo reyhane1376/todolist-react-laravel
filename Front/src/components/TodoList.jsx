77ffc690d38d3128cfe3ae8bfd59bfb1192bb483
reyhane <97376622+reyhane1376@users.noreply.github.com>
2024-11-03 19:45:14 +0330

    add edit and fix gitignore

--- Front/src/components/TodoList.jsx
+++ Front/src/components/TodoList.jsx
@@ -1,12 +1,12 @@
-import TodoListItem from "./TodoListItem"
+import TodoListItem from "./TodoListItem";
 
 
+export default function TodoList({ todos , deleteTodo , toggleTodoStatus , editTodoTitle}) {
 
-export default function TodoList({ todos, deleteTodo, toggleTodoStatusHandler })
-{
+
     return (
         <ul className="list-reset">
-            { todos.map( (todo, index) => <TodoListItem key={index} todo={todo} deleteTodo={deleteTodo} toggleTodoStatusHandler={toggleTodoStatusHandler} />) }
+            { todos.map( (todo, index) => <TodoListItem key={index} todo={todo} deleteTodo={deleteTodo} toggleTodoStatus={toggleTodoStatus} editTodoTitle={editTodoTitle} />) }
         </ul>
     )
 }
--- Front/src/components/Todos.jsx
+++ Front/src/components/Todos.jsx
@@ -1,77 +1,92 @@
-import { useState } from "react"
-import TodoList from "./TodoList"
-import NoWorkResult from "postcss/lib/no-work-result";
+import { useState } from "react";
+import TodoList from "./TodoList";
+import { v4 as uuidv4 } from 'uuid';
 
-export default function Todos () 
-{
-    const [ todos, setTodos] = useState([
+export default function Todos() {
+    const [ todos , setTodos ] = useState([
         {
-            id : 1,
-            title : 'go school',
-            status : false
+            id : uuidv4(),
+            title : 'go to school and read books',
+            status: true
         },
         {
-            id : 2,
-            title : 'go to gym',
-            status : true
+            id : uuidv4(),
+            title : 'go to gym at 17:00',
+            status : false
         }
     ]);
+    const [ newTodoTitle , setNewTodoTitle ] = useState("");
 
-    const addTodoNewHandler = (event) => {
-        if (event.key === 'Enter' && event.target.value !== "") {
+
+
+    const onInputNewTodoChangeHandler = (event) => {
+        setNewTodoTitle(event.target.value)
+    }
+
+    const addNewTodoHandler = (event) => {
+        if( event.key === 'Enter' && newTodoTitle !== "") {
+
             setTodos([
                 ...todos,
                 {
-                    id : 3,
-                    title : event.target.value,
-                    status : false
+                    id : uuidv4(),
+                    title : newTodoTitle,
+                    status : false,
                 }
             ])
 
-            event.target.value = '';
+            setNewTodoTitle("");
         }
     }
 
+    const deleteTodoHandler = (todo) => {
+        let newTodos = todos.filter( (todoItem) => {
+            return todo.id != todoItem.id;
+        })
 
-    const deleteTodo = (todo) => {
-        let newTodo = todos.filter( (todoItem) => {
-            return todo.id !== todoItem.id
-        });
-
-
-        setTodos(newTodo)
+        setTodos(newTodos);
     }
 
     const toggleTodoStatusHandler = (todo) => {
-
-        let newTodo = todos.map( (todoItem) => {
-            if (todo.id == todoItem.id) {
+        let newTodos = todos.map( (todoItem) =>  {
+            if(todo.id === todoItem.id) {
                 todoItem.status = ! todoItem.status
             }
-
 
             return todoItem;
         })
 
+        setTodos(newTodos);
+    }
 
-        setTodos(newTodo);
+    const editTodoTitleHandler = (todo , newTitleValue ) => {
+        let newTodos = todos.map( (todoItem) =>  {
+            if(todo.id === todoItem.id) {
+                todoItem.title = newTitleValue
+            }
 
+            return todoItem;
+        })
+
+        setTodos(newTodos);
     }
 
 
     return (
         <div className="flex items-center justify-center h-screen">
-        <div className="w-full px-4 py-8 mx-auto shadow lg:w-1/3  bg-white">
-            <div className="flex items-center mb-6">
-                <h1 className="mr-6 text-4xl font-bold text-purple-600"> TO DO APP</h1>
+            <div className="w-full px-4 py-8 mx-auto shadow lg:w-1/3  bg-white">
+                <div className="flex items-center mb-6">
+                    <h1 className="mr-6 text-4xl font-bold text-purple-600"> TO DO APP</h1>
+                </div>
+                <div className="relative">
+                    <input type="text" placeholder="What needs to be done today?"
+                        onChange={onInputNewTodoChangeHandler}
+                        onKeyDown={addNewTodoHandler}
+                        value={newTodoTitle}
+                        className="w-full px-2 py-3 border rounded outline-none border-grey-600" />
+                </div>
+                <TodoList todos={todos} deleteTodo={deleteTodoHandler} toggleTodoStatus={toggleTodoStatusHandler} editTodoTitle={editTodoTitleHandler}/>
             </div>
-            <div className="relative">
-                <input type="text" placeholder="What needs to be done today?"
-                onKeyDown={addTodoNewHandler}
-                className="w-full px-2 py-3 border rounded outline-none border-grey-600" />
-            </div>
-            <TodoList todos={todos} deleteTodo={deleteTodo} toggleTodoStatusHandler={toggleTodoStatusHandler} />
         </div>
-    </div>
     )
 }
