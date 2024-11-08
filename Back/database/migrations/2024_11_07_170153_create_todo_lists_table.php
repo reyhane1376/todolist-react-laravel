fe84c0dd6044df30dccd6b06e2f0696fcccb47c8
reyhane <97376622+reyhane1376@users.noreply.github.com>
2024-11-08 10:41:09 +0330

    add factory for todolist

--- Back/database/migrations/2024_11_07_170153_create_todo_lists_table.php
+++ Back/database/migrations/2024_11_07_170153_create_todo_lists_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('todo_lists', function (Blueprint $table) {
+        Schema::create('todolists', function (Blueprint $table) {
             $table->id();
             $table->string('title');
             $table->boolean('status');
