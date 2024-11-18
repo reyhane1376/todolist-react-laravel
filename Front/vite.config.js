835e41f97b6e19ebeb947b35fcd40940e3dfdd0e
reyhane <reyhaneebrahimi27@yahoo.com>
2024-11-18 19:04:37 +0330

    fix docker for react app

--- Front/vite.config.js
+++ Front/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',  // Make Vite accessible externally
+    port: 5173,        // Ensure the port matches the one in docker-compose
+  },
 })
